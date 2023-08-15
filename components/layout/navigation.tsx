50f6d2fdafb32b3bfdde61fdc02f0c1d88a1afcd
AKSHAY N <94807520+akshaynstack@users.noreply.github.com>
2023-08-16 00:50:11 +0530

    Update navigation.tsx

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -34,36 +34,37 @@ const Navigation: React.FC = () => {
   }, [mobileNav.isOpen]);
 
   return (
-    <HStack spacing="2" flexShrink={0}>
-      {siteConfig.header.links.map(({ href, id, ...props }, i) => {
-        return (
-          <NavLink
-            display={["none", null, "block"]}
-            href={href || `/#${id}`}
-            key={i}
-            isActive={
-              !!(
-                (id && activeId === id) ||
-                (href && !!router.asPath.match(new RegExp(href)))
-              )
-            }
-            {...props}
-          >
-            {props.label}
-          </NavLink>
-        );
-      })}
+    // <HStack spacing="2" flexShrink={0}>
+    //   {siteConfig.header.links.map(({ href, id, ...props }, i) => {
+    //     return (
+    //       <NavLink
+    //         display={["none", null, "block"]}
+    //         href={href || `/#${id}`}
+    //         key={i}
+    //         isActive={
+    //           !!(
+    //             (id && activeId === id) ||
+    //             (href && !!router.asPath.match(new RegExp(href)))
+    //           )
+    //         }
+    //         {...props}
+    //       >
+    //         {props.label}
+    //       </NavLink>
+    //     );
+    //   })}
 
-      <ThemeToggle />
+    //   <ThemeToggle />
 
-      <MobileNavButton
-        ref={mobileNavBtnRef}
-        aria-label="Open Menu"
-        onClick={mobileNav.onOpen}
-      />
+    //   <MobileNavButton
+    //     ref={mobileNavBtnRef}
+    //     aria-label="Open Menu"
+    //     onClick={mobileNav.onOpen}
+    //   />
 
-      <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose} />
-    </HStack>
+    //   <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose} />
+    // </HStack>
+    <></>
   );
 };
 
