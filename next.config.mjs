7dc8f9ddc9e66f4d9da13b39543e60cdce96353e
AKSHAY N <94807520+akshaynstack@users.noreply.github.com>
2023-08-16 01:05:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -26,4 +26,14 @@ const nextConfig = {
   },
 }
 
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
+
 export default withContentlayer(nextConfig)
